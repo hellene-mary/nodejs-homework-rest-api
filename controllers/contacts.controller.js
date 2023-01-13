440cd03f163fb8162fd435447ef0ebb56e40849c
Grek Maria <grekmariaa@gmail.com>
2023-01-14 00:01:12 +0200

    is done

---
+++ controllers/contacts.controller.js
@@ -0,0 +1,83 @@
+const { Contact } = require("../models/contacts")
+
+async function getAllContacts(req, res) {
+  const allContacts = await Contact.find()
+  res.status(200).json(allContacts)
+}
+
+async function getContactById(req, res) {
+  const { contactId } = req.params
+  const contactById = await Contact.findById(contactId)
+
+  if (!contactById) {
+    res.status(404).json({ message: "Not found" })
+    return
+  }
+
+  res.status(200).json(contactById)
+}
+
+async function addContact(req, res) {
+  const { name, email, phone } = req.body
+
+  const NewContact = await Contact.create({ name, email, phone })
+  console.log("NewContact", NewContact)
+
+  res.status(201).json(NewContact)
+}
+
+async function removeContact(req, res) {
+  const { contactId } = req.params
+  const response = await Contact.findById(contactId)
+
+  if (!response) {
+    res.status(404).json({ message: "Not found" })
+    return
+  }
+
+  await Contact.findByIdAndRemove(contactId)
+  return res.status(200).json({ message: "Contact deleted" })
+}
+
+async function updateContact(req, res) {
+  const { contactId } = req.params
+  const upContact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  })
+  console.log("upContact", upContact)
+  if (!upContact) {
+    res.status(404).json({ message: "Not found" })
+    return
+  }
+  res.status(200).json(upContact)
+}
+
+async function updateStatusContact(req, res) {
+  const { contactId } = req.params
+  const { favorite } = req.body
+
+  if (!favorite) {
+    console.log("missing field favorite")
+    res.status(400).json({ message: "missing field favorite" })
+    return
+  }
+
+  const upContact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  })
+
+  if (!upContact) {
+    return res.status(404).json({ message: "Not found" })
+  }
+  res.status(200).json(upContact)
+  console.log("upContact", upContact)
+}
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+  updateStatusContact,
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,18 @@
 const express = require("express")
-const { nanoid } = require("nanoid")
+
 const {
   validateNewContact,
   validateUpdateContact,
 } = require("../../middlewares/validateContacts")
 
 const {
-  listContacts,
+  getAllContacts,
   getContactById,
   addContact,
   removeContact,
   updateContact,
-} = require("../../models/contacts")
-
+  updateStatusContact,
+} = require("../../controllers/contacts.controller")
 const {
   addContactSchema,
   putContactSchema,
@@ -20,64 +20,15 @@ const {
 
 const router = express.Router()
 
-router.get("/", async (req, res, next) => {
-  const allContacts = await listContacts()
-  res.status(200).json(allContacts)
-})
-
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params
-  const contactById = await getContactById(contactId)
-
-  if (!contactById) {
-    res.status(404).json({ message: "Not found" })
-    return
-  }
-
-  res.status(200).json(contactById)
-})
-
-router.post(
-  "/",
-  validateNewContact(addContactSchema),
-  async (req, res, next) => {
-    const { name, email, phone } = req.body
-    const body = {
-      id: nanoid(),
-      name: name,
-      email: email,
-      phone: phone,
-    }
-    addContact(body)
-
-    res.status(201).json(body)
-  }
-)
-
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params
-  const response = await removeContact(contactId)
-
-  if (!response) {
-    res.status(404).json({ message: "Not found" })
-    return
-  }
-
-  res.status(200).json({ message: "contact deleted" })
-})
-
+router.get("/", getAllContacts)
+router.get("/:contactId", getContactById)
+router.post("/", validateNewContact(addContactSchema), addContact)
+router.delete("/:contactId", removeContact)
 router.put(
   "/:contactId",
   validateUpdateContact(putContactSchema),
-  async (req, res, next) => {
-    const { contactId } = req.params
-    const response = await updateContact(contactId, req.body)
-    if (!response) {
-      res.status(404).json({ message: "Not found" })
-      return
-    }
-    res.status(200).json(response)
-  }
+  updateContact
 )
+router.patch("/:contactId/favorite", updateStatusContact)
 
 module.exports = router
