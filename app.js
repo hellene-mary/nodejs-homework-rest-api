24ad6a43adbd1ff865bc95c0ed3b997cfc7e91d0
Grek Maria <grekmariaa@gmail.com>
2023-01-26 16:55:19 +0200

    step 1
    static routes

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const express = require("express")
-const logger = require("morgan")
-const cors = require("cors")
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts")
-const { authRouter } = require("./routes/api/auth")
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
+const { authRouter } = require("./routes/api/auth");
+const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short"
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/contacts", contactsRouter)
-app.use("/api/users", authRouter)
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
+app.use("/public", express.static("public"));
 
 app.use((req, res) => {
-  return res.status(404).json({ message: "Not found!" })
-})
+  return res.status(404).json({ message: "Not found!" });
+});
 
 app.use((err, req, res, next) => {
   if (err.message.includes("Cast to ObjectId failed for value")) {
     return res.status(400).json({
       message: "id is invalid",
-    })
+    });
   }
 
-  return res.status(err.status || 500).json({ message: err.message })
-})
+  return res.status(err.status || 500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
